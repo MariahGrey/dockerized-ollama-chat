3ba2d49ed7093d3e931aaea1846d50b959a4fff0
mariah@echobind.com <mariah@echobind.com>
2025-03-07 12:49:47 -0800

    correct structure

--- dockerize-ollama-app/prisma/migrations/20250307192619_add_response_to_queries/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `response` to the `Query` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Query" ADD COLUMN     "response" TEXT NOT NULL;
